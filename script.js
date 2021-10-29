ed4f4b41126e5eafdbf5d9e8f88d6215be7c7629
Sebastian Amezquita <samezquita2605@gmail.com>
2021-10-29 10:36:18 -0400

    changed author

--- script.js
+++ script.js
@@ -15,22 +15,22 @@ const songs = [
     {
       name: 'jacinto-1',
       displayName: 'Electric Chill Machine',
-      artist: 'Jacinto Design',
+      artist: 'Unknown',
     },
     {
       name: 'jacinto-2',
       displayName: 'Seven Nation Army (Remix)',
-      artist: 'Jacinto Design',
+      artist: 'Unknown',
     },
     {
       name: 'jacinto-3',
       displayName: 'Goodnight, Disco Queen',
-      artist: 'Jacinto Design',
+      artist: 'Unknown',
     },
     {
       name: 'metric-1',
       displayName: 'Front Row (Remix)',
-      artist: 'Metric/Jacinto Design',
+      artist: 'Unknown',
     },
   ];
 
